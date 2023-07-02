16cf042c8f79a35f6824d3e2029e8b76b32967a8
Renzo Figueroa <renzofigueroa1999@gmail.com>
2023-07-01 22:24:04 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Renzo Rodrigo Figueroa Fernández on 1/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
